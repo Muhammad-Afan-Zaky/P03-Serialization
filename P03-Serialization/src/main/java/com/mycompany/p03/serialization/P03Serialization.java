96250572768e4a2c25ce5ff5cd0714e9bd4939a8
Muhammad Afan Zaky <117789019+Muhammad-Afan-Zaky@users.noreply.github.com>
2024-03-31 12:10:31 +0700

    Add files via upload

---
+++ P03-Serialization/src/main/java/com/mycompany/p03/serialization/P03Serialization.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.p03.serialization;
+
+/**
+ *
+ * @author User
+ */
+public class P03Serialization {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
